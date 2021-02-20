9b3f199c54cbff14945f8488ffee5e0f58c1ac9f
Adam Rasfeld <arasfeld@gmail.com>
2021-02-20 11:01:31 -0500

    dont instantiate new prisma clients on hot reload

--- graphql/context.ts
+++ graphql/context.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { PrismaClient } from '@prisma/client'
+import type { PrismaClient } from '@prisma/client'
 import { getLoginSession, Session } from '../lib/session'
+import prisma from '../prisma/client'
 
 export type Context = {
   prisma: PrismaClient
@@ -8,8 +9,6 @@ export type Context = {
   res: NextApiResponse
   session?: Session
 }
-
-const prisma = new PrismaClient()
 
 export const createContext = async ({ req, res }: Context): Promise<Context> => {
   const session = await getLoginSession(req)
